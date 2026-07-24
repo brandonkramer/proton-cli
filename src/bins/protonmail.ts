65880a7530188f5e40eafd793a46759ea837db1a
brandonkramer <4xsquid@gmail.com>
2026-07-24 02:16:30 +0100

    revert: remove Bridge Mail in favor of unified API sign-in
    Drop the IMAP/SMTP Bridge path so Mail can join proton signin multi-mint
    with shared key unlock; API Mail will land in a follow-up.

--- src/bins/protonmail.ts
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bun
-/**
- * Compatibility wrapper: protonmail … → proton mail …
- */
-const args = process.argv.slice(2);
-const forwarded = ["mail", ...args];
-process.argv = [process.argv[0]!, process.argv[1]!, ...forwarded];
-await import("../index.ts");
